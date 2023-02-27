47095e8e41322ee27959b723ce94ab39c0708a8f
AkshayV30 <akshayatwork.v30@gmail.com>
2023-02-28 01:26:23 +0530

    dockerfile-update

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,3 @@
 FROM nginx
 COPY . /usr/share/nginx/html/
 WORKDIR "/usr/share/nginx/html/"
-EXPOSE 80 
---
+++ main.tf
@@ -0,0 +1,9 @@
+# manual step wwithout load balancing
+# create resources in ec2
+# install ami :amazon linux
+# configure firewall
+# install docker
+# run docker image from docker hub 
+# --which is pushed with github actions too docker hub
+
+# output ec2 instance id
